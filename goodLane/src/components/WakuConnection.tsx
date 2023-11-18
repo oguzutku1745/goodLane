fee14abee4771f26ff82ed7f25a081322042508d
nhestrompia <nhestrompia@gmail.com>
2023-11-18 13:02:38 +0300

    feat: add waku example

---
+++ goodLane/src/components/WakuConnection.tsx
@@ -0,0 +1,82 @@
+import {
+  LightNode,
+  createDecoder,
+  createEncoder,
+  createLightNode,
+  waitForRemotePeer,
+} from "@waku/sdk";
+import protobuf from "protobufjs";
+import React, { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+
+interface IWakuConnection {}
+
+const contentTopic = "/light-guide/1/message/proto";
+
+// Create a message encoder and decoder
+const encoder = createEncoder({ contentTopic });
+const decoder = createDecoder(contentTopic);
+
+const messageType = new protobuf.Type("ChatMessage")
+  .add(new protobuf.Field("timestamp", 1, "uint64"))
+  .add(new protobuf.Field("sender", 2, "string"))
+  .add(new protobuf.Field("message", 3, "string"));
+
+const WakuConnection: React.FC<IWakuConnection> = ({}) => {
+  //   const [messageStructure, setMessageStructure] = useState<protobuf.Type>();
+  const [wakuNode, setWakuNode] = useState<LightNode | null>(null);
+  const [text, setText] = useState("");
+
+  const { address } = useAccount();
+  console.log("🚀 ~ file: WakuConnection.tsx:13 ~ address:", address);
+
+  const createWakuConnection = async () => {
+    const node = await createLightNode({ defaultBootstrap: true });
+    console.log(
+      "🚀 ~ file: WakuConnection.tsx:35 ~ createWakuConnection ~ node:",
+      node
+    );
+    await node.start();
+    await waitForRemotePeer(node);
+    setWakuNode(node);
+  };
+
+  const sendMessage = async () => {
+    const protoMessage = messageType!.create({
+      timestamp: Date.now(),
+      sender: "Alice",
+      message: "Hello, World!",
+    });
+
+    const serialisedMessage = messageType!.encode(protoMessage).finish();
+
+    await wakuNode!.lightPush.send(encoder, {
+      payload: serialisedMessage,
+    });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  useEffect(() => {
+    const initializeWaku = async () => {
+      await createWakuConnection();
+    };
+    initializeWaku();
+  }, []);
+
+  return (
+    <div className="flex gap-5">
+      <input onChange={(e) => handleChange(e)} type="text" value={text} />
+      <button
+        onClick={sendMessage}
+        className="px-4 py-2 border rounded-md"
+        type="submit"
+      >
+        Send
+      </button>
+    </div>
+  );
+};
+export default WakuConnection;
--- goodLane/src/pages/index.tsx
+++ goodLane/src/pages/index.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import GrantCard from '@/components/GrantCard'
-import { useState, useEffect } from 'react';
-
+import GrantCard from "@/components/GrantCard";
+import WakuConnection from "@/components/WakuConnection";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [campaigns, setCampaigns] = useState([]);
@@ -10,38 +8,37 @@ export default function Home() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-      const fetchCampaigns = async () => {
-          try {
-              const response = await fetch('/api/campaigns');
-              if (!response.ok) {
-                  throw new Error(`Error: ${response.status}`);
-              }
-              const data = await response.json();
-              setCampaigns(data.campaigns);
-          } catch (error : any) {
-              setError(error.message);
-          } finally {
-              setLoading(false);
-          }
-      };
+    const fetchCampaigns = async () => {
+      try {
+        const response = await fetch("/api/campaigns");
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+        const data = await response.json();
+        setCampaigns(data.campaigns);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-      fetchCampaigns();
+    fetchCampaigns();
   }, []);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
-  console.log(campaigns)
+  console.log(campaigns);
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <GrantCard />
-
-    </div>
-
+        {/* <WakuConnection /> */}
+      </div>
     </main>
-  )
+  );
 }
