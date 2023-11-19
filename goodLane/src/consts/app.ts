3c82c02cd440da8558d7f948e03077eedb75097d
Oğuz Utku Yıldız <oguzutku1745@gmail.com>
2023-11-19 04:26:34 +0300

    src removed

--- goodLane/src/consts/app.ts
+++
@@ -1,3 +0,0 @@
-export const APP_NAME = 'Hyperlane Warp UI Template';
-export const APP_DESCRIPTION = 'A DApp for Hyperlane Warp Route transfers';
-export const APP_URL = 'hyperlane-warp-template.vercel.app';
--- goodLane/src/pages/router.tsx
+++
@@ -1,7 +0,0 @@
-import {TransferTokenCard} from '../../../hyperlane-warp-ui-template/src/features/transfer/TransferTokenCard';
-
-export default function Router () {
-    return(
-        <TransferTokenCard />
-    )
-}
